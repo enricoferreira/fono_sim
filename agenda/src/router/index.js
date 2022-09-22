5975dc6e0da1b952d470c8fc1d52aebd6cd5e6e8
Enrico Ferreira <enricoferreira41@gmail.com>
2022-09-21 23:02:24 -0300

    install router

---
+++ agenda/src/router/index.js
@@ -0,0 +1,7 @@
+import { routes } from "./routes"
+
+export const router = VueRouter.createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: VueRouter.createWebHashHistory(),
+    routes, // short for `routes: routes`
+  })
