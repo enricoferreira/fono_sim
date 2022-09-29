18d698608c21e73b767da495d1db0d35a75e49d2
Enrico Ferreira <enricoferreira41@gmail.com>
2022-09-29 10:39:05 -0300

    arrumando agenda

--- agenda/src/views/ViewAgenda.vue
+++ agenda/src/views/ViewAgenda.vue
@@ -1,10 +1,24 @@
 <template>
-  <v-container>Agenda</v-container>
+  <v-container>
+    <v-tabs  grow color="primary" v-model="tab">
+      <v-tab value="por_dia">Calendário</v-tab>
+      <v-tab value="por_colab">Colaboradores</v-tab>
+    </v-tabs>
+
+    <v-window v-model="tab">
+      <v-window-item value="por_dia">
+        Por dia
+      </v-window-item>
+      <v-window-item value="por_colab">
+        Por colab
+      </v-window-item>
+    </v-window>
+  </v-container>
 </template>
 
 <script setup>
 import {useMainStore} from '../store/main'
-import { onMounted } from '@vue/runtime-core'
+import { onMounted, ref } from '@vue/runtime-core'
 import { storeToRefs } from 'pinia'
 
 const store_main = useMainStore()
@@ -14,6 +28,9 @@ const {changeTitlePage} = storeToRefs(store_main)
 onMounted(() => {
   store_main.changeTitlePage('Agenda')
 })
+
+const tab = ref('por_dia')
+
 
 </script>
 
